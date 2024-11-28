657b3ae63499e7666aa917b4928955cf63355009
SiliconAnalysts <contact@siliconanalysts.com>
2024-11-27 23:20:52 -0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import FoundryCapacityOptimizer from './components/FoundryCapacityOptimizer';
+
+function App() {
+  return (
+    <div className="App">
+      <FoundryCapacityOptimizer />
+    </div>
+  );
+}
+
+export default App;
